a0c4df955e8e2f4ff339181cd5234a4802ffd3f2
hungtk <kieuhungcm2015@gmail.com>
2024-07-17 10:20:06 +0700

    fixed

--- src/middlewares/session.middleware.ts
+++ src/middlewares/session.middleware.ts
@@ -1,5 +1,5 @@
 import expressSession, { Session } from "express-session";
-import redisStore from "../databases/redis.database";
+// import redisStore from "../databases/redis.database";
 import dotenv from "dotenv";
 import { EXPRESS_SESSION_MAX_AGE } from "./constant";
 
@@ -9,19 +9,23 @@ export interface SessionData extends Session {
   userId?: string;
 }
 
+// export const getSession = async (req): Promise<SessionData> => {
+//   let sessionData: SessionData;
+//   await redisStore.get(req.headers.authentication, (err, session) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     sessionData = session;
+//   });
+//   return sessionData;
+// };
 export const getSession = async (req): Promise<SessionData> => {
-  let sessionData: SessionData;
-  await redisStore.get(req.headers.authentication, (err, session) => {
-    if (err) {
-      console.log(err);
-    }
-    sessionData = session;
-  });
+  const sessionData: SessionData = req.session;
   return sessionData;
 };
 
 const session = expressSession({
-  store: redisStore,
+  // store: redisStore,
   secret: process.env.EXPRESS_SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
